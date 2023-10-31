73348b314a2c41a9e8211cb7db1c69a26d871851
Sk-uni-lab <skomar300@caledonian.ac.uk>
2023-10-31 11:30:58 +0000

    simple adding app

---
+++ addNum.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.text.DecimalFormat;
+
+public class addNum {
+    public static void main(String[] args) {
+
+        int n1, n2,tn;
+        n1 = 3;
+        n2= 2;
+        tn = n1 + n2;
+        System.out.println("the number should be 5, the number is: " + tn);
+
+       
+    }
+
+}
